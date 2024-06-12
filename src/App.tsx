530fbb87e7576c06bb776acbd815b05642d924b9
Shriram kushwaha <117708615+shri214@users.noreply.github.com>
2024-06-12 22:06:18 +0530

    done

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { TodoList } from './components/Todo/todoList';
+
+function App() {
+  return (
+    <div className="App">
+      <TodoList/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Function/add.ts
@@ -0,0 +1,16 @@
+import { Todo } from '../data/dataDef';
+
+
+
+export const addTodo = (todos: Todo[], newTodoText: string): Todo[] => {
+  if (newTodoText.trim()) {
+    const newTodoItem: Todo = {
+      id: Date.now(),
+      text: newTodoText,
+      completed: false,
+      inProgress: false,
+    };
+    return [...todos, newTodoItem];
+  }
+  return todos;
+};
